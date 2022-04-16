1339900318363b35e524b58d2985ad13aac0dfc6
huiyu-y <iamyu418@gmail.com>
2022-04-16 00:00:36 -0400

    seperate-video

--- functions0.js
+++ functions0.js
@@ -30,6 +30,18 @@ var swiper10 = new Swiper(".swiper10", {
   plugins: [SwiperPluginNavigation]
 });
 
+var swiper11 = new Swiper(".swiper11", {
+  mousewheel: {
+    invert: false,
+    interval: 400
+  },
+  navigation: {
+    prevEl: ".navigation-prevEl11",
+    nextEl: ".navigation-nextEl11"
+  },
+  plugins: [SwiperPluginNavigation]
+});
+
 
 
 AnalogClock("clock", {
@@ -138,6 +150,10 @@ const playerennoPlayer = new Vimeo.Player('playerenno', options);
 const playernnnPlayer = new Vimeo.Player('playernnn', options);
 
 const playerchinaPlayer = new Vimeo.Player('playerchina', options);
+
+const playerduckPlayer = new Vimeo.Player('playerduck', options);
+
+
 
 function _removeEmojis(str) {
   return str.replace(/([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?/g, '');
